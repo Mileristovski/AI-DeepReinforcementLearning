a371479557ae91b81ef1c6ba0fd3a6d887203f5d
Mile Ristovski <mristovski@myges.fr>
2025-02-03 16:07:33 +0100

    [ADD] - Ajoute de Grid World

--- src/bin/testing.rs
+++ src/bin/testing.rs
@@ -2,13 +2,13 @@ mod services;
 mod environments;
 
 use crossterm::terminal::disable_raw_mode;
-use crate::environments::line_world::LineEnv;
 use crate::services::testing::envs::testing_env_manually;
 use crate::services::testing::common::{clear_screen, end_of_run, user_choice};
 
 pub fn main() {
     let options = vec![
         "Line World",
+        "Grid World",
         "Quit"
     ];
 
@@ -18,8 +18,9 @@ pub fn main() {
         clear_screen();
 
         match selected_index {
-            0 => { testing_env_manually(&mut LineEnv::new()); },
-            1 => { break; }
+            0 => { testing_env_manually(&mut environments::line_world::LineEnv::new()); },
+            1 => { testing_env_manually(&mut environments::grid_world::GridEnv::new()); },
+            2 => { break; }
             _ => {}
         }
         end_of_run();
