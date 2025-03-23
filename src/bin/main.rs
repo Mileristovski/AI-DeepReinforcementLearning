9ab5a43240d0de1e073371552c847f68704a61ce
MileRistovski <ristovski.mile19@gmail.com>
2025-03-23 18:58:28 +0100

    [FIX] - Made the main.rs a bit cleaner

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,25 +8,29 @@ use crate::services::testing::envs::testing_env_manually;
 use crate::services::testing::common::{clear_screen, end_of_run, user_choice};
 
 fn main() {
-    let options = vec![
-        "Line World",
-        "Grid World",
-        "Tic Tac Toe",
-        "Bobeil",
-        "Quit"
-    ];
-
+    let mut from_random = false;
     let mut selected_index = 0;
+
     loop {
+        let options = vec![
+            if from_random { "Random is on, turn random off" } else { "Random is off, Turn random on" },
+            "Line World",
+            "Grid World",
+            "Tic Tac Toe",
+            "Bobeil",
+            "Quit"
+        ];
+
         selected_index = user_choice(options.clone());
         clear_screen();
 
         match selected_index {
-            0 => { testing_env_manually(&mut environments::line_world::LineEnv::new(), options[selected_index]); },
-            1 => { testing_env_manually(&mut environments::grid_world::GridEnv::new(), options[selected_index]); },
-            2 => { testing_env_manually(&mut environments::tic_tac_toe::TicTacToeEnv::new(), options[selected_index]); },
-            3 => { testing_env_manually(&mut environments::bobail::BobailEnv::new(), options[selected_index]); },
-            4 => { break; }
+            0 => { from_random = !from_random; }
+            1 => { testing_env_manually(&mut environments::line_world::LineEnv::new(), options[selected_index], from_random); },
+            2 => { testing_env_manually(&mut environments::grid_world::GridEnv::new(), options[selected_index], from_random); },
+            3 => { testing_env_manually(&mut environments::tic_tac_toe::TicTacToeEnv::new(), options[selected_index], from_random); },
+            4 => { testing_env_manually(&mut environments::bobail::BobailEnv::new(), options[selected_index], from_random); },
+            5 => { break; }
             _ => {}
         }
         end_of_run();
