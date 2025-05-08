fce6a28dc587eb02a5fac14c241b0b186a0b168f
MileRistovski <ristovski.mile19@gmail.com>
2025-05-08 19:37:44 +0200

    [FIX] - Made debug a testing ground for envs

--- src/bin/debug.rs
+++ src/bin/debug.rs
@@ -3,8 +3,41 @@ mod environments;
 mod algorithms;
 mod config;
 
-use crate::environments::env::Env;
+use crate::environments::env::DeepDiscreteActionsEnv;
+use crate::environments::grid_world::GridWorld;
+
+type GameEnv = GridWorld;
+
+fn display_properties(env: &mut GameEnv) {
+    println!("{:?}", env.state_description());
+    println!("{:?}", env.board);
+    println!("{:?}", env.available_actions_ids().collect::<Vec<_>>());
+    println!("{:?}", env.action_mask());
+}
 
 fn main() {
+    let mut env = GameEnv::default();
+    // env.is_random_state = true;
+    env.reset();
+    
+    println!("{}", env);
+    display_properties(&mut env);
+    env.step(1);
+    display_properties(&mut env);
+    println!("{}", env);
+    
+    env.step(0);
+    display_properties(&mut env);
+    println!("{}", env);
+        
+    env.step(3);
+    display_properties(&mut env);
+    println!("{}", env);
+
+    env.step(3);
+    display_properties(&mut env);
+    println!("{}", env);
+    
+    display_properties(&mut env);
     unimplemented!("This should be used as a test script")
 }
