946c107161e6586f92948e931cd0a25b9d4a9926
MileRistovski <ristovski.mile19@gmail.com>
2025-05-10 00:00:43 +0200

    [FIX] - Fixed main rust file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,7 +7,7 @@ mod config;
 use gui::cli::common::{clear_screen, user_choice};
 use gui::cli::submenu::submenu;
 use crossterm::terminal::disable_raw_mode;
-use environments::tic_tac_toe::{TicTacToeVersusRandom, TTT_NUM_ACTIONS, TTT_NUM_STATE_FEATURES};
+use environments::tic_tac_toe::{TicTacToeVersusRandom, TTT_NUM_ACTIONS, TTT_NUM_STATE_FEATURES, TTT_NUM_STATES};
 use environments::line_world::{LineWorld, LINE_NUM_ACTIONS, LINE_NUM_STATE_FEATURES};
 use environments::grid_world::{GridWorld, GRID_NUM_ACTIONS, GRID_NUM_STATE_FEATURES};
 use environments::bobail::{ BobailHeuristic, BB_NUM_ACTIONS, BB_NUM_STATE_FEATURES};
@@ -27,10 +27,10 @@ fn main() {
         clear_screen();
 
         match selected_index {
-            0 => { submenu::<LINE_NUM_STATE_FEATURES, LINE_NUM_ACTIONS, LineWorld>(&mut LineWorld::default(), options[selected_index]); },
-            1 => { submenu::<GRID_NUM_STATE_FEATURES, GRID_NUM_ACTIONS, GridWorld>(&mut GridWorld::default(), options[selected_index]); },
-            2 => { submenu::<TTT_NUM_STATE_FEATURES, TTT_NUM_ACTIONS, TicTacToeVersusRandom>(&mut TicTacToeVersusRandom::default(), options[selected_index]); },
-            3 => { submenu::<BB_NUM_STATE_FEATURES, BB_NUM_ACTIONS, BobailHeuristic>(&mut BobailHeuristic::default(), options[selected_index]); },
+            0 => { submenu::<LINE_NUM_STATE_FEATURES, LINE_NUM_ACTIONS,LINE_NUM_STATE_FEATURES, LineWorld>(&mut LineWorld::default(), options[selected_index]); },
+            1 => { submenu::<GRID_NUM_STATE_FEATURES, GRID_NUM_ACTIONS,GRID_NUM_STATE_FEATURES, GridWorld>(&mut GridWorld::default(), options[selected_index]); },
+            2 => { submenu::<TTT_NUM_STATE_FEATURES, TTT_NUM_ACTIONS, TTT_NUM_STATES, TicTacToeVersusRandom>(&mut TicTacToeVersusRandom::default(), options[selected_index]); },
+            3 => { submenu::<BB_NUM_STATE_FEATURES, BB_NUM_ACTIONS, BB_NUM_STATE_FEATURES, BobailHeuristic>(&mut BobailHeuristic::default(), options[selected_index]); },
             4 => { break; }
             _ => {}
         }
